b1ea50c7b66efeab983767ae29b8b3ac34a46cc4
mosheur <mosheur.rahman.15@gmail.com>
2021-09-08 05:14:41 +0600

    backup

--- index.js
+++ index.js
@@ -2,9 +2,59 @@
 import nodemailer from 'nodemailer'
 import commander from 'commander'
 import chalk from 'chalk'
+import fs from 'fs'
 import input from './lib/promptQuery.js'
 import loadJson from './lib/jsonLoader.js'
 import validators from './lib/validators.js'
+
+
+async function parseArgs(args) {
+    let parsed = {}
+    
+    if (args.sender){
+        let list = args.sender.split(':').filter(a=>a)
+        let addr = list[0].trim()
+        let pass = args.sender.replace(list[0]+':', '')
+        
+        if (!validators.validateAddress(addr)){
+            console.log(`${ chalk.red('Error:') } invalid email address '${ addr }'`)
+            process.exit()
+        }
+        
+        if (!pass){
+            pass = await input.password(`password for (${ addr })`)
+        }
+        
+        parsed.sender = { addr, pass }
+    }
+    else {
+        parsed.sender = {
+            addr: await input.sender ('sender address'),
+            pass: await input.password ('password'),
+        }
+    }
+    
+    if (args.reciver){
+        let tmp = []
+        for (let item in args.reciver){
+            let is_file = validators.pathStat(item).file
+            let is_addr = validators.validateAddress(item)
+            if (!(is_file || is_addr)){
+                console.log(`${chalk.red('Error:')} '${ item }' is not a valid address or a filename.`)
+                process.exit
+            }
+            if (is_file && is_addr){
+                console.log(`'${ item }' is a valid address also a file path.`)
+                process.exit()
+            }
+            if (is_file){
+                let buff = fs.readFileSync(item, 'utf-8')
+                let list = buff.split('\n').filter(a=>a.trim())
+                tmp      = [ ...tmp, ...list]
+            }
+        }
+    }
+}
 
 async function main() {
     const settings = loadJson('./package.json') // load package.json
@@ -13,44 +63,11 @@ async function main() {
     
     program.version(version)
     .option('-s, --sender <address>', 'sender email address.')
-    .option('-t, --target <address...>', 'target email addresses.')
+    .option('-r, --reciver <address...>', 'reciver email addresses.')
     .option('-S, --save', 'save login info to avoid entering password.')
     .parse(process.argv)
     
-    const args = program.opts()
-    
-    // sender validation.
-    if (!args.sender) {
-        args.sender = {
-            addr: await input.sender('sender address'),
-            pass: await input.password('password')
-        }
-    }
-    else {
-        // split password and addr. remove emtpy item with filter if 
-        // password is empty, example: 'addr@something.com:'
-        // in the addr there is no password but sep ':' (colon) has been passed.
-        // in this case split will add a empty item  [''] in the list. remove it with filter.
-        let list = args.sender.split(':').filter(i=>i) 
-        let tmp  = {}
-        
-        // validate email address.
-        if (!validators.validateAddress(list[0].trim())){
-            console.log(`${chalk.red('Error:')} invalid email '${ list[0] }'`)
-            process.exit()
-        }
-        
-        tmp.addr = list[0].trim()
-        
-        // parse password if passed with command line arguments.
-        // example : 'address@someting.com:my_password'
-        // prompt for password otherwise.
-        if (list.length>1) tmp.pass = args.sender.replace(list[0]+':', '')   // list[0] => email address
-        else tmp.pass = await input.password(`password for (${ tmp.addr })`)
-        
-        args.sender = tmp
-    }
-    
+    const args = parseArgs(program.opts())
     console.log(args)
 }
 
